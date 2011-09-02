e54fba3271b47794a6de4aa86af02dc6c96b1f7e
kamekoopa <hogehugo@gmail.com>
2011-09-02 19:03:04 +0900

    フレームワーク例外実装

--- core/exception/FrameworkException.php
+++ core/exception/FrameworkException.php
@@ -1,9 +1,22 @@
-<?php namespace framework\core\expcetion;
+<?php namespace core\expcetion;
 
 /**
  * フレームワークの基本例外
  *
  * @author kmaitani
  */
-class core_exception_FrameworkException extends Exception{
+class FrameworkException extends Exception{
+
+
+	/**
+	 * コンストラクタ
+	 * @access public
+	 *
+	 * @param int       $statusCode この例外をHTTPステータスコードとして表した時の値
+	 * @param string    $message    エラーメッセージ
+	 * @param Exception $cause      この例外の原因となった例外
+	 */
+	public function __construct($statusCode = 500, $message = "something error occurred.", Exception $cause = null){
+		parent::__construct($message, $statusCode, $cause);
+	}
 }
